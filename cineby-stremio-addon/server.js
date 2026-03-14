f4f4d59c570101e7020b81ac84d8fecf20108a1a
Rutunjay Sonune <rutunjaysonune309@gmail.com>
2026-03-14 22:58:58 +0530

    Update server.js

--- cineby-stremio-addon/server.js
+++ cineby-stremio-addon/server.js
@@ -1,7 +1,7 @@
 const { addonBuilder, serveHTTP } = require("stremio-addon-sdk")
 const https = require("https")
 
-const TMDB_API = "e4598ac9cb6d28883dac12852c670c5a"
+const TMDB_API_KEY = "PASTE_YOUR_TMDB_API_KEY_HERE"
 
 const manifest = {
     id: "org.cineby.vidking",
@@ -18,7 +18,7 @@ const builder = new addonBuilder(manifest)
 function imdbToTmdb(imdbId) {
     return new Promise((resolve, reject) => {
 
-        const url = `https://api.themoviedb.org/3/find/${imdbId}?api_key=${TMDB_API}&external_source=imdb_id`
+        const url = `https://api.themoviedb.org/3/find/${imdbId}?api_key=${TMDB_API_KEY}&external_source=imdb_id`
 
         https.get(url, (res) => {
 
@@ -27,52 +27,58 @@ function imdbToTmdb(imdbId) {
             res.on("data", chunk => data += chunk)
 
             res.on("end", () => {
+                try {
+                    const json = JSON.parse(data)
 
-                const json = JSON.parse(data)
+                    if (json.movie_results && json.movie_results.length > 0)
+                        resolve(json.movie_results[0].id)
 
-                if (json.movie_results.length > 0)
-                    resolve(json.movie_results[0].id)
+                    else if (json.tv_results && json.tv_results.length > 0)
+                        resolve(json.tv_results[0].id)
 
-                else if (json.tv_results.length > 0)
-                    resolve(json.tv_results[0].id)
+                    else
+                        resolve(null)
 
-                else
+                } catch (err) {
                     resolve(null)
-
+                }
             })
 
-        }).on("error", reject)
+        }).on("error", () => resolve(null))
 
     })
 }
 
 builder.defineStreamHandler(async ({ type, id }) => {
 
-    const imdb = id.split(":")[0]
+    const imdbId = id.split(":")[0]
 
-    const tmdb = await imdbToTmdb(imdb)
+    const tmdbId = await imdbToTmdb(imdbId)
 
-    if (!tmdb)
+    if (!tmdbId)
         return { streams: [] }
 
-    let url
+    let streamUrl
 
     if (type === "movie") {
-        url = `https://www.vidking.net/embed/movie/${tmdb}`
+
+        streamUrl = `https://www.vidking.net/embed/movie/${tmdbId}`
+
     } else {
 
         const parts = id.split(":")
         const season = parts[1]
         const episode = parts[2]
 
-        url = `https://www.vidking.net/embed/tv/${tmdb}/${season}/${episode}`
+        streamUrl = `https://www.vidking.net/embed/tv/${tmdbId}/${season}/${episode}`
+
     }
 
     return {
         streams: [
             {
                 title: "Cineby (Vidking)",
-                url: url
+                url: streamUrl
             }
         ]
     }
